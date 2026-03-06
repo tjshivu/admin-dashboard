49966330e9c7b605595a3c9b876141085158fe85
Shivu <100021616+shivutj@users.noreply.github.com>
2026-03-07 00:29:38 +0530

    build problem solved (website config updated)

--- website/next.config.ts
+++ website/next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname, "../../"),
+  outputFileTracingRoot: path.join(__dirname, "../"),
   turbopack: {
-    root: path.join(__dirname, "../../"),
+    root: path.join(__dirname, "../"),
   },
   // Compression
   compress: true,
